badc077169fea7b5aa19a15c4d95c66ad5b8dd13
Mariusz Zieliński <git@zielma.com>
2025-01-26 00:11:48 +0100

    refactor: env configuration to read proper variables

--- internal/config/config.go
+++ internal/config/config.go
@@ -3,24 +3,42 @@ package config
 import (
 	"errors"
 	"os"
+	"strings"
 )
 
 type Config struct {
-	YNABAPIKey       string
-	GoCardlessAPIKey string
+	YNABAPIKey             string
+	GoCardlessClientID     string
+	GoCardlessClientSecret string
 }
+
+const (
+	EnvYnabApiKey             = "YAGI_YNAB_API_KEY"
+	EnvGoCardlessClientID     = "YAGI_GOCARDLESS_CLIENT_ID"
+	EnvGoCardlessClientSecret = "YAGI_GOCARDLESS_CLIENT_SECRET"
+)
+
+var (
+	ErrYNABAPIKeyNotSet             = errors.New(EnvYnabApiKey + " is not set")
+	ErrGoCardlessClientIDNotSet     = errors.New(EnvGoCardlessClientID + " is not set")
+	ErrGoCardlessClientSecretNotSet = errors.New(EnvGoCardlessClientSecret + " is not set")
+)
 
 func NewFromEnv() (*Config, error) {
 	c := Config{
-		YNABAPIKey:       os.Getenv("YAGI_YNAB_API_KEY"),
-		GoCardlessAPIKey: os.Getenv("YAGI_GOCARDLESS_API_KEY"),
+		YNABAPIKey:             os.Getenv(EnvYnabApiKey),
+		GoCardlessClientID:     os.Getenv(EnvGoCardlessClientID),
+		GoCardlessClientSecret: os.Getenv(EnvGoCardlessClientSecret),
 	}
 
-	if c.YNABAPIKey == "" {
-		return nil, errors.New("YNAB_API_KEY is not set")
+	if strings.TrimSpace(c.YNABAPIKey) == "" {
+		return nil, ErrYNABAPIKeyNotSet
 	}
-	if c.GoCardlessAPIKey == "" {
-		return nil, errors.New("YAGI_GOCARDLESS_API_KEY is not set")
+	if strings.TrimSpace(c.GoCardlessClientID) == "" {
+		return nil, ErrGoCardlessClientIDNotSet
+	}
+	if strings.TrimSpace(c.GoCardlessClientSecret) == "" {
+		return nil, ErrGoCardlessClientSecretNotSet
 	}
 
 	return &c, nil
